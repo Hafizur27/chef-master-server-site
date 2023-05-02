37e2303d9aef9789a631cc3ab4a67cfc02a48735
hafizur27 <mahmadhafiz33@gmail.com>
2023-05-02 21:44:44 +0600

    server site is ready for project

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+app.get('/', (req, res) =>{
+    res.send('server is ready')
+})
+
+app.listen(port, () =>{
+    console.log(`chef api is running on port: ${port}`)
+})
