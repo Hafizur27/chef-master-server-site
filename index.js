08c071ba2b7c2ffcaa90cf54d0629f0419233178
hafizur27 <mahmadhafiz33@gmail.com>
2023-05-03 13:54:13 +0600

    make fake categories data send for server

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
+const categories = require('./data/chefsCategories.json')
 
 app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('server is ready')
+})
+app.get('/categories', (req, res) =>{
+    res.send(categories)
 })
 
 app.listen(port, () =>{
