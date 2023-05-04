562804d963f07a67f1148a6614fd7f75d7acec04
hafizur27 <mahmadhafiz33@gmail.com>
2023-05-04 22:43:39 +0600

    server site done

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
-const categories = require('./data/chefsCategories.json')
+const categories = require('./data/chefsCategories.json');
+const chefs = require('./data/chefs.json');
 
 app.use(cors());
 
@@ -11,6 +12,20 @@ app.get('/', (req, res) =>{
 })
 app.get('/categories', (req, res) =>{
     res.send(categories)
+})
+app.get('/chefs',(req, res) =>{
+    res.send(chefs)
+})
+app.get('/chefs/:id',(req, res) =>{
+    const id = req.params.id;
+    const selectedChefs = chefs.find(n =>n._id ==id)
+    res.send(selectedChefs)
+})
+app.get('/categories/:id',(req,res)=>{
+    const id = req.params.id;
+    console.log(id)
+    const categoryChefs = chefs.filter(n =>n.category_id ==id)
+    res.send(categoryChefs)
 })
 
 app.listen(port, () =>{
